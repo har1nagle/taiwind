db7352ac42e3fc0e62f03332358e0cef0d918b46
har1nagle <hnagle.0@yahoo.com>
2023-08-08 08:42:26 +0530

    add toggle feature in header file on menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai"
 
 
 function Header() {
+  const [toggle, setToggle] = useState(false);
   return (
     <div className='bg-sky-600 p-4'>
       <div className='max-w-[1240px] items-center flex justify-between mx-auto'>
         <div className='text-2xl font-bold'>
           Portfolio
         </div>
-        <AiOutlineMenu className='text-white text-2xl md:hidden block'/>
-        {/* <AiOutlineClose className='text-white text-2xl md:hidden block' /> */}
+        {
+          toggle ?
+            <AiOutlineClose onClick={()=> setToggle(!toggle)} className='text-white text-2xl md:hidden block' />
+          :
+            <AiOutlineMenu onClick={()=> setToggle(!toggle)} className='text-white text-2xl md:hidden block'/>
+
+
+        }
         <ul className='hidden md:flex text-white gap-5'>
           <li> Home </li>
           <li> About </li>
@@ -18,7 +25,8 @@ function Header() {
           <li> Carrer </li>
         </ul>
 
-        <ul className='w-full h-screen md:hidden text-white gap-5 fixed bg-black left-0 top-[63px]'>
+        <ul className={`w-full h-screen md:hidden text-white gap-5 fixed bg-black top-[63px] 
+          ${toggle ? 'left-[0]': 'left-[-100%]'}`}>
           <li className='p-5'> Home </li>
           <li className='p-5'> About </li>
           <li className='p-5'> Contacts </li>
